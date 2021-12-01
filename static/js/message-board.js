63284e1c96b231162cc414744d95e182fa9ec8e8
PasseRR <xie__hai@sina.com>
2021-12-01 12:43:04 +0800

    js修改

--- static/js/message-board.js
+++ static/js/message-board.js
@@ -1,26 +1,20 @@
 // fork from https://github.com/YangHanqing/yanghanqing.github.io
 $(document).ready(function () {
+    setComment();
+})
+
+function getIssuesUrl() {
+    var user = $('meta[name="author"]').attr("content");
+    var issueId = $('meta[name="message_board_issue_id"]').attr("content");
+    return 'https://api.github.com/repos/' + user + '/' + user + '.github.io/issues/' + issueId;
+}
+
+function setComment() {
     var issuesUrl = getIssuesUrl();
 
     $("#commentsList").removeAttr('data_comments_url');
     $("#tips").html("我们不会获取您的用户名和密码,评论直接通过 HTTPS 与 Github API交互,<br>如果您开启了两步验证,请在博客的<a  target=\"_blank\" href=\"" + issuesUrl + "\">Github issues</a>下添加留言");
-    setComment(issuesUrl);
-})
 
-function getIssueId() {
-    return $('meta[name="message_board_issue_id"]').attr("content");
-}
-
-function getUser() {
-    return $('meta[name="author"]').attr("content");
-}
-
-function getIssuesUrl() {
-    var user = getUser();
-    return 'https://api.github.com/repos/' + user + '/' + user + '.github.io/issues/' + getIssueId();
-}
-
-function setComment(issuesUrl) {
     $("#comments").show();
     $("#commentsList").children().remove();
     $("#commentsList").removeAttr('data_comments_url');
@@ -57,58 +51,34 @@ function login() {
 
 function subComment() {
     var USERNAME = $("#txt_username").val();
-    var PASSWORD = document.getElementById("txt_password").value; //
-    // 未开启评论
-    if (typeof ($("#commentsList").attr("data_comments_url")) == "undefined") {
-        var createIssueJson = "{\"title\": \"" + title + "\"}";
-        $.ajax({
-            type: "POST",
-            url: getIssuesUrl(),
-            dataType: 'json',
-            async: false,
-            headers: {
-                "Authorization": "Basic " + btoa(USERNAME + ":" + PASSWORD)
-            },
-            data: createIssueJson,
-            success: function () {
-                // console.log('开启评论成功:' + title);
-                //重新遍历issue list
-                setComment(getIssuesUrl());
-            }
-        });
-    }
+    var PASSWORD = document.getElementById("txt_password").value;
     // console.log("准备提交评论");
-    // 已开启评论
-    if (typeof ($("#commentsList").attr("data_comments_url")) != "undefined") {
-        var issueURL = $("#commentsList").attr("data_comments_url");
-        var comment = $("#comment_txt").val();
-        var commentJson = "{\"body\": \"" + comment + "\"}";
-        // console.log(comment);
-        if (comment == "") {
-            alert("评论不能为空");
-            return;
-        }
-
-        $.ajax({
-            type: "POST",
-            url: issueURL,
-            dataType: 'json',
-            async: false,
-            headers: {
-                "Authorization": "Basic " + btoa(USERNAME + ":" + PASSWORD)
-            },
-            data: commentJson,
-            success: function () {
-                // 更新评论区
-                setComment(getIssuesUrl());
-
-                $("#comment_txt").val('');
-            },
-            error: function (XMLHttpRequest, textStatus, errorThrown) {
-                alert("账号密码错误,或者开启了两步验证");
-            }
-        });
-    } else {
-        console.log("未开启评论")
+    var issueURL = $("#commentsList").attr("data_comments_url");
+    var comment = $("#comment_txt").val();
+    var commentJson = "{\"body\": \"" + comment + "\"}";
+    // console.log(comment);
+    if (comment == "") {
+        alert("评论不能为空");
+        return;
     }
+
+    $.ajax({
+        type: "POST",
+        url: issueURL,
+        dataType: 'json',
+        async: false,
+        headers: {
+            "Authorization": "Basic " + btoa(USERNAME + ":" + PASSWORD)
+        },
+        data: commentJson,
+        success: function () {
+            // 更新评论区
+            setComment();
+
+            $("#comment_txt").val('');
+        },
+        error: function (XMLHttpRequest, textStatus, errorThrown) {
+            alert("账号密码错误,或者开启了两步验证");
+        }
+    });
 }
